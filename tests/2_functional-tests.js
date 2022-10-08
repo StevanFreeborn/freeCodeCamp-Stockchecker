69c9b223b7f5ad14026f2dd68e4bb534d911c582
Stevan Freeborn <sfreeborn7@gmail.com>
2022-10-08 16:38:20 -0500

    scaffold functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,24 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', () => {
+    test('Can get one stock.', done => {
 
+    });
+
+    test('Can get one stock and like the stock.', done => {
+
+    });
+
+    test('Can\'t like same stock twice', done => {
+
+    });
+
+    test('Can get two stocks.', done => {
+
+    });
+
+    test('Can get two stocks and like both stocks.', done => {
+
+    });
 });
