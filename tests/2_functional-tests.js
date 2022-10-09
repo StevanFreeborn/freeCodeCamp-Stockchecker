e1ac14a0c6f08725c65f15654383fb2dc726c47c
StevanFreeborn <sfreeborn7@gmail.com>
2022-10-09 10:59:39 -0500

    added test for getting two stocks

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,6 +8,7 @@ chai.use(chaiHttp);
 suite('Functional Tests', () => {
 
     const testStockOneName = 'vti';
+    const testStockTwoName = 'vtsax';
 
     test('Can get one stock.', (done) => {
         chai.request(server)
@@ -123,8 +124,38 @@ suite('Functional Tests', () => {
     });
 
     test('Can get two stocks.', done => {
-        assert.fail();
-        done();
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({ 
+            stock: testStockOneName,
+            stock: testStockTwoName,
+        })
+        .end((err, res) => {
+            if (err) console.log(err)
+            
+            assert.equal(res.status, 200);
+            assert.equal(res.type, 'application/json');
+            
+            assert.isObject(res.body);
+            assert.property(res.body, 'stockData');
+            
+            const stockData = res.body?.stockData;
+            assert.isArray(stockData);
+            assert.equal(stockData?.length, 2);
+
+            stockData?.forEach(stock => {
+                assert.property(stock, 'stock');
+                assert.isString(stock?.stock);
+    
+                assert.property(stock, 'price');
+                assert.isNumber(stock?.price);
+    
+                assert.property(stock, 'likes');
+                assert.isNumber(stock?.likes);
+            })
+
+            done();
+        });
     });
 
     test('Can get two stocks and like both stocks.', done => {
