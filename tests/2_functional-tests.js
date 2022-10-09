6ca7964d3720d5812d2df7f95a2329f615adae14
StevanFreeborn <sfreeborn7@gmail.com>
2022-10-09 11:30:58 -0500

    added test for getting and liking two stocks

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -140,6 +140,7 @@ suite('Functional Tests', () => {
             assert.property(res.body, 'stockData');
             
             const stockData = res.body?.stockData;
+
             assert.isArray(stockData);
             assert.equal(stockData?.length, 2);
 
@@ -159,7 +160,54 @@ suite('Functional Tests', () => {
     });
 
     test('Can get two stocks and like both stocks.', done => {
-        assert.fail();
-        done();
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({ 
+            stock: testStockOneName,
+            stock: testStockTwoName,
+        })
+        .end((err, res) => {
+            if (err) console.log(err)
+            const firstStockOriginalLikes = res.body?.stockData[0]?.likes;
+            const secondStockOriginalLikes = res.body?.stockData[1]?.likes;
+
+            chai.request(server)
+            .get('/api/stock-prices')
+            .query({ 
+                stock: testStockOneName,
+                stock: testStockTwoName,
+                like: true,
+            })
+            .end((err, res) => {
+                if (err) console.log(err)
+
+                assert.equal(res.status, 200);
+                assert.equal(res.type, 'application/json');
+                
+                assert.isObject(res.body);
+                assert.property(res.body, 'stockData');
+                
+                const stockData = res.body?.stockData;
+
+                assert.isArray(stockData);
+                assert.equal(stockData?.length, 2);
+    
+                stockData?.forEach(stock => {
+                    assert.property(stock, 'stock');
+                    assert.isString(stock?.stock);
+        
+                    assert.property(stock, 'price');
+                    assert.isNumber(stock?.price);
+        
+                    assert.property(stock, 'likes');
+                    assert.isNumber(stock?.likes);
+                })
+                
+                assert.equal(stockData[0]?.likes, firstStockOriginalLikes + 1);
+                assert.equal(stockData[1]?.likes, secondStockOriginalLikes + 1);
+                
+                done();
+            })
+        });
     });
 });
