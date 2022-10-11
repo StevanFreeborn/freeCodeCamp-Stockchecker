905cd81ddb878f23713a889bff5129c284cfdd78
Stevan Freeborn <sfreeborn7@gmail.com>
2022-10-11 10:59:27 -0500

    begin working on connecting to databases

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 const stockRoutes = require('./routes/stocks');
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
@@ -32,7 +34,6 @@ app.route('/')
   });
 
 fccTestingRoutes(app);
-
 stockRoutes(app);
 
 app.use((req, res, next) => {
@@ -42,6 +43,29 @@ app.use((req, res, next) => {
 });
 
 // TODO: Connect to database on start-up.
+
+let mongoConnectionString; 
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    mongoConnectionString = process.env.MONGO_DEV_URI
+    break;
+  case 'test':
+    mongoConnectionString = process.env.MONGO_TEST_URI
+    break;
+  default:
+    mongoConnectionString = process.env.MONGO_PROD_URI
+    break;
+}
+
+mongoose
+.connect(mongoConnectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  console.log(`Mongoose connected successfully to ${proccess.env.NODE_ENV} database.`);
+})
+.catch(() => {
+
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
